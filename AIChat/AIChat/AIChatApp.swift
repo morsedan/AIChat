69df5c2849273a1d1ebd4f383d37a0dd2988c8e2
Dan Morse <djmthedev@gmaill.com>
2025-01-09 14:25:41 -0800

    Added project.

---
+++ AIChat/AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Morse on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChat/AIChatTests/AIChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Morse on 1/9/25.
+//
+
+import Testing
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
