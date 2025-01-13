6039ff93b55ce683fa87d059566bd6dcaeeb1a55
Dan Morse <djmthedev@gmaill.com>
2025-01-13 07:46:01 -0800

    Added onboarding intro view and onboarding color view.

---
+++ AIChat/AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  AIChat
+//
+//  Created by Morse on 1/13/25.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
